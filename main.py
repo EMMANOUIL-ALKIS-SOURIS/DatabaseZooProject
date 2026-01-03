d3d69c3efc716df585ce922c9e80899e435a558b
Manos <manos.souris@outlook.com>
2026-01-03 17:42:40 +0200

    First approach on the specific ID search

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import terminalFeatures as gui
 
 def run_app():
     
-    # 1. Open the connection once for the entire session
+    # 1.Open the connection once for the entire session
     db_connection = connect_to_db()
 
     if not db_connection:
@@ -12,30 +12,36 @@ def run_app():
 
     try:
 
-        # 2. Get user identification
+        # 2.Get user identification
         user_id, user_name = gui.user_login(db_connection)
         is_admin = (user_id is False) #is_admin is True when the user inputs '0' and thus user_is is False
 
-        # 3. Dynamic Welcome Message
+        # 3.Dynamic Welcome Message
         if is_admin:
             print("\nWelcome Admin. Accessing full database...")
         else:
             #Fetch and display the specific name for employee user
-            #user_name = gui.get_user_name(db_connection, user_id)
             print(f"\nWelcome {user_name}!")
 
-        # 4. Main Program Loop - Menu and Choices
+        # 4.Main Program Loop - Menu and Choices
         while True:
             choice = gui.validate_menu_choice(is_admin)
             
-            if choice == "1":
-                print("\n[Search with ID selected]")
-            elif choice == "2":
-                print("\n[Statistics selected]")
-            elif choice == "3":
-                print("\n[Manage Enclosures selected]")
-            elif choice == "4":
-                print("\n[Employee Records selected]")
+            match choice:
+                case "0":
+                    print("Exiting program...")
+                    exit(0)
+                case "1":
+                    #print("\n[Search with ID selected]")
+                    user_input = input("Enter the ID to search for:\n")
+                    res = gui.search_by_id(db_connection, user_input)
+                    print(f"{res}")
+                case "2":
+                    print("\n[Statistics selected]")
+                case "3":
+                    print("\n[Manage Enclosures selected]")
+                case "4":
+                    print("\n[Employee Records selected]")
             
             input("\nPress Enter to return to the menu...")
 
--- terminalFeatures.py
+++ terminalFeatures.py
@@ -31,12 +31,7 @@ def validate_menu_choice(adminFlag):
     while True:
         choice, valid_keys = print_menu(adminFlag)
 
-        if choice in valid_keys:
-            match choice:
-                case "0":
-                    print("Exiting program...")
-                    exit(0)
-                
+        if choice in valid_keys:                
             return choice
 
         print(f"\nInvalid input. Please select from: {', '.join(sorted(valid_keys))}")
@@ -57,7 +52,7 @@ def user_login(connection):
                 result = cursor.fetchone()
                 
                 if result:
-                    # Success: ID exists, and we have the name
+                    #ID exists, and we have the name
                     return user_input, result[0]
                 else:
                     print(f"Access Denied: ID {user_input} not found in our records.")
@@ -65,3 +60,153 @@ def user_login(connection):
                 print(f"Database error: {e}")
         else:
             print("Invalid ID. Please provide a valid employee ID.")
+
+
+def search_by_id(connection, entity_id):
+    
+    if len(entity_id) == 6 and entity_id.startswith('8') and entity_id.isdigit():
+        #Fetch the ticket's details from the database using the provided ticket_id
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM Ticket WHERE Ticket_ID = ?", (entity_id,))
+            result = cursor.fetchall()
+            
+            if result:
+                #ID exists
+                return result[0]
+            else:
+                print(f"ID {entity_id} not found in our records.")
+
+        except Exception as e:
+            print(f"Database error: {e}")
+
+    elif len(entity_id) == 6 and entity_id.startswith('9') and entity_id.isdigit():
+        #Fetch the event's details from the database using the provided event_id
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM Event WHERE Event_ID = ?", (entity_id,))
+            result = cursor.fetchall()
+            
+            if result:
+                #ID exists
+                return result[0]
+            else:
+                print(f"ID {entity_id} not found in our records.")
+
+        except Exception as e:
+            print(f"Database error: {e}")
+
+    elif len(entity_id) == 6 and entity_id.startswith('10') and entity_id.isdigit():
+        #Fetch the animal's details from the database using the provided animal_id
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM Animal WHERE Animal_ID = ?", (entity_id,))
+            result = cursor.fetchall()
+            
+            if result:
+                #ID exists
+                return result[0]
+            else:
+                print(f"ID {entity_id} not found in our records.")
+
+        except Exception as e:
+            print(f"Database error: {e}")
+
+    elif len(entity_id) == 6 and entity_id.startswith('12') and entity_id.isdigit():
+        #Fetch the contract's details from the database using the provided contract_id
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM Contract WHERE Contract_ID = ?", (entity_id,))
+            result = cursor.fetchall()
+            
+            if result:
+                #ID exists
+                return result[0]
+            else:
+                print(f"ID {entity_id} not found in our records.")
+
+        except Exception as e:
+            print(f"Database error: {e}")
+
+    elif len(entity_id) == 6 and entity_id.startswith('15') and entity_id.isdigit():
+        #Fetch the diet's details from the database using the provided diet_id
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM Diet WHERE Diet_ID = ?", (entity_id,))
+            result = cursor.fetchall()
+            
+            if result:
+                #ID exists
+                return result[0]
+            else:
+                print(f"ID {entity_id} not found in our records.")
+
+        except Exception as e:
+            print(f"Database error: {e}")
+
+    elif len(entity_id) == 6 and entity_id.startswith('20') and entity_id.isdigit():
+        #Fetch the employee's details from the database using the provided employee_id
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM Employee WHERE Employee_ID = ?", (entity_id,))
+            result = cursor.fetchall()
+            
+            if result:
+                #ID exists
+                return result[0]
+            else:
+                print(f"ID {entity_id} not found in our records.")
+
+        except Exception as e:
+            print(f"Database error: {e}")
+    
+    elif len(entity_id) == 6 and entity_id.startswith('30') and entity_id.isdigit():
+        #Fetch the food's details from the database using the provided food_id
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM Food WHERE Food_ID = ?", (entity_id,))
+            result = cursor.fetchall()
+            
+            if result:
+                #ID exists
+                return result[0]
+            else:
+                print(f"ID {entity_id} not found in our records.")
+
+        except Exception as e:
+            print(f"Database error: {e}")
+
+    elif len(entity_id) == 6 and entity_id.startswith('44') and entity_id.isdigit():
+        #Fetch the area's details from the database using the provided area_id
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM Area WHERE Area_ID = ?", (entity_id,))
+            result = cursor.fetchall()
+            
+            if result:
+                #ID exists
+                return result[0]
+            else:
+                print(f"ID {entity_id} not found in our records.")
+
+        except Exception as e:
+            print(f"Database error: {e}")
+
+    elif len(entity_id) == 6 and entity_id.startswith('54') and entity_id.isdigit():
+        #Fetch the habitat's details from the database using the provided habitat_id
+        try:
+            cursor = connection.cursor()
+            cursor.execute("SELECT * FROM Habitat WHERE Habitat_ID = ?", (entity_id,))
+            result = cursor.fetchall()
+            
+            if result:
+                #ID exists
+                return result[0]
+            else:
+                print(f"ID {entity_id} not found in our records.")
+
+        except Exception as e:
+            print(f"Database error: {e}")
+    
+    else:
+        print("Invalid ID format. Please ensure the ID is correct.")
